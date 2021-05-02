74510cb39ad028bb8ea5bf66e028f2f04e788f01
Julio Cesar Rosetim <jrosetim@gmail.com>
2021-05-02 10:15:39 -0300

    Projeto teste funcionando

---
+++ javaDocker/src/main/java/com/javaDocker/controller/PessoaController.java
@@ -0,0 +1,16 @@
+package com.javaDocker.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/pessoa")
+public class PessoaController {
+
+    @GetMapping("/getAll")
+    public String getPessoa(){
+        return "Ola Mundo Teste";
+    }
+}
