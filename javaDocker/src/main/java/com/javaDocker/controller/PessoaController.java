435523e84f82f5ff3294c1912324a1915abe700a
Julio Cesar Rosetim <jrosetim@gmail.com>
2021-05-02 12:40:07 -0300

    alteração do retorno da pessoa e adicionado readme.md

--- javaDocker/src/main/java/com/javaDocker/controller/PessoaController.java
+++ javaDocker/src/main/java/com/javaDocker/controller/PessoaController.java
@@ -1,16 +1,24 @@
 package com.javaDocker.controller;
 
+import com.javaDocker.DTO.PessoaDto;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/pessoa")
+@RequestMapping("/pessoas")
 public class PessoaController {
 
+    PessoaDto pessoaDto;
+
     @GetMapping("/getAll")
-    public String getPessoa(){
-        return "Ola Mundo Teste";
+    public PessoaDto getPessoa(){
+        pessoaDto = new PessoaDto();
+        pessoaDto.setNome("Teste");
+        pessoaDto.setCnpjcpf("05589483988");
+
+        return pessoaDto;
     }
+
 }
